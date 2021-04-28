b934b2f54eafb6eed77990bb5d0cdcd17adc26a7
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-04-28 18:15:28 -0400

    Fixed console error on shipping screen when filling in the shipping form inputs.

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -8,7 +8,7 @@ const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
 
-  const [address, setAddress] = useState(shippingAddress.address);
+  const [address, setAddress] = useState(shippingAddress.address || "");
   const [city, setCity] = useState(shippingAddress.city);
   const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
   const [country, setCountry] = useState(shippingAddress.country);
