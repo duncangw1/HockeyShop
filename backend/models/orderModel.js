7a2bb6effe1d2c51c53a0035e5bb9464ee204437
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-05-01 11:33:28 -0400

    Added shipping address state to the orderModel for the database to record the user's entered state

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -23,6 +23,7 @@ const orderSchema = mongoose.Schema(
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
+      state: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
