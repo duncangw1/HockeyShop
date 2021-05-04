fbaa4b9ccf5edfe1278900104d60c50ee70cb678
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-05-04 11:52:15 -0400

    Created orders reset on logout to prevent exposing previously logged in user's orders

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -15,6 +15,7 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -53,8 +54,9 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
-  dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -53,7 +53,7 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     case USER_DETAILS_RESET:
-      return {};
+      return { user: {} };
     default:
       return state;
   }
